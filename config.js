8d2adb9a94f0424b2916f430816a2028c399b7bb
Hélio Mauricio Vieira <hmauricio@stverginia.local>
2026-06-11 12:07:45 -0300

    Painel comboista tablet com KPIs abastecimento e lubrificacao

---
+++ config.js
@@ -0,0 +1,5 @@
+window.SB_URL = "https://azhpxhrwhegfysoeqmft.supabase.co";
+window.SB_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6aHB4aHJ3aGVnZnlzb2VxbWZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NzUxODcsImV4cCI6MjA5NDI1MTE4N30.iQU1T1NLaGIQyqScLS6qNaoo1QWcI8Mh-jjN52TU5to";
+window.CAP_COMBOIO = 5000;
+window.REFRESH_MS = 30000;
